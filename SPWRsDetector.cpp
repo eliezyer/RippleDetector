2a4f6aecba50cf561c0c748292a53a3883c41c07
eliezyer <fermino.bioeng@gmail.com>
2016-07-19 14:12:58 -0300

    Update SPWRsDetector.cpp

--- SPWRsDetector.cpp
+++ SPWRsDetector.cpp
@@ -70,6 +70,7 @@ void SPWRsDetector::addModule()
     m.AvgCount = 0;
     m.flag = 0;
     m.tReft = 0.0;
+    m.count = 0;
     
     
     modules.add(m);
@@ -229,18 +230,21 @@ void SPWRsDetector::process(AudioSampleBuffer& buffer,
                     break;
                 }
             }
-            int count = 0;
+
             for (int i = 0; i < arrSize; i++)
             {
             
                 const float sample = RMS[i];
-                double threshold = module.MED + 2*sqrt(module.STD/(module.AvgCount*4));
+                double threshold = module.MED + 2.00*sqrt(module.STD/(module.AvgCount*4));
                 
-                if ( sample >=  threshold  )
+                if ( sample >=  threshold & RefratTime > 0.2 )
                 {
-                  count++;
+                  module.count++;
                 }
-                
+		else if(sample < threshold & i == 0)//protect from acumulation
+		{
+		  module.count = 0;
+ 		}               
                 if (module.flag == 1)
                 {
                     t3 = ( double(time.getHighResolutionTicks()) / double(time.getHighResolutionTicksPerSecond()) )- module.tReft;//clock() - module.tReft;
@@ -251,18 +255,19 @@ void SPWRsDetector::process(AudioSampleBuffer& buffer,
                     RefratTime = 0.3;
                 }
        
-                if (count == round(arrSize*0.60) & RefratTime > 0.2 )
+                if (module.count >= round(0.020*30000/4) & RefratTime > 0.2 ) // o original qdo utilizado com buffer de 1024 pontos é um tamanho de 20 ms (154/(30000/4))
                 {
                         module.flag = 1;
+			module.count = 0;
                         module.tReft = double(time.getHighResolutionTicks()) / double(time.getHighResolutionTicksPerSecond());
-                        double timeStamp2 = (module.tReft - t); 
+                        //double timeStamp2 = (module.tReft - t); 
                         
-                        //salving the script delay
-                        FILE *f = fopen("timeStamps.txt", "a+");
+                        //saving the script delay
+                 //       FILE *f = fopen("timeStamps.txt", "a+");
                         
-                        fprintf(f,"%f\n ",timeStamp2*1000);
+                 //       fprintf(f,"%f\n ",timeStamp2*1000);
 
-                        fclose(f); 
+                 //       fclose(f); 
                         
     
                         addEvent(events, TTL, i, 1, module.outputChan);
